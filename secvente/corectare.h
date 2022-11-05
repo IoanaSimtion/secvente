9b93d750dfb06fe82dc4b50626a5447753cd7d5b
Ioana Simtion <ionitza6@gmail.com>
2022-11-05 15:55:11 +0200

    added header,text file

---
+++ secvente/corectare.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "func.h"
+
+//cerinta: Se dă un șir cu n elemente, numere întregi. Determinați secvența de elemente cu suma maximă.
+
+void rezolvare() {
+	int v[100], n;
+	citireVector(v, n);
+
+}
